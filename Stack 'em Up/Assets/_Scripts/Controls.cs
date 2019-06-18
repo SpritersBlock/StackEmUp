020f5699a64fe6ac55438f1c0cdaad0f3b5ee8c2
Connor Isogai <MXCapturing@gmail.com>
2019-06-18 13:15:29 +1200

    Updated Unity Version + UMVP Made
    + Text Mesh Pro
    + Winner is U

---
+++ Stack 'em Up/Assets/_Scripts/Controls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Controls : MonoBehaviour {
+
+    public TMP_Text text;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Win");
+            text.text += "Winner is U ";
+        }
+	}
+}
