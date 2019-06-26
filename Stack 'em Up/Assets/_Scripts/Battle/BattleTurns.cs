da3d0f43f93d7bf0340d2e5c6db173cc00ca8e90
Connor Isogai <MXCapturing@gmail.com>
2019-06-26 13:49:59 +1200

    Script Change

--- Stack 'em Up/Assets/_Scripts/Battle/BattleTurns.cs
+++ Stack 'em Up/Assets/_Scripts/Battle/BattleTurns.cs
@@ -7,42 +7,49 @@ public class BattleTurns : MonoBehaviour {
 
     public TMP_Text text;
     public int turnNo;
+    bool joust;
 
     private void Start()
     {
         turnNo = 0;
+        joust = false;
     }
 
     private void FixedUpdate()
     {
-        text.gameObject.transform.position = BattleManager.instance.playerFieldList[turnNo].transform.position - new Vector3(-2, 0, 0);
-        text.text = turnNo.ToString();
-        if (turnNo == 0)
+        if(joust == false)
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
+            text.gameObject.transform.position = BattleManager.instance.playerFieldList[turnNo].transform.position - new Vector3(-2, 0, 0);
+            text.text = turnNo.ToString();
+            if (turnNo == 0)
             {
-                turnNo++;
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    turnNo++;
+                }
             }
-        }
-        if (turnNo == 1)
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha2))
+            if (turnNo == 1)
             {
-                turnNo++;
+                if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    turnNo++;
+                }
             }
-        }
-        if (turnNo == 2)
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha2))
+            if (turnNo == 2)
             {
-                StartCoroutine("Joust");
+                if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    StartCoroutine("Joust");
+                }
             }
         }
     }
 
     IEnumerator Joust()
     {
+        joust = true;
         yield return new WaitForSeconds(2f);
         turnNo = 0;
+        joust = false;
     }
 }
