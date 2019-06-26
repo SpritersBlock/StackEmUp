409a26e000b8d812226bf54b9e7d906dd6f37b8e
Connor Isogai <MXCapturing@gmail.com>
2019-06-26 14:00:23 +1200

    Merge

---
+++ Stack 'em Up/Assets/PROTOTYPE/_Scripts/Battle/BattleTurns.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BattleTurns : MonoBehaviour {
+
+    public TMP_Text text;
+    public int turnNo;
+    bool joust;
+
+    private void Start()
+    {
+        turnNo = 0;
+        joust = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if(joust == false)
+        {
+            text.gameObject.transform.position = BattleManager.instance.playerFieldList[turnNo].transform.position - new Vector3(-2, 0, 0);
+            text.text = turnNo.ToString();
+            if (turnNo == 0)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1))
+                {
+                    turnNo++;
+                }
+            }
+            if (turnNo == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    turnNo++;
+                }
+            }
+            if (turnNo == 2)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    StartCoroutine("Joust");
+                }
+            }
+        }
+    }
+
+    IEnumerator Joust()
+    {
+        joust = true;
+        yield return new WaitForSeconds(2f);
+        turnNo = 0;
+        joust = false;
+    }
+}
