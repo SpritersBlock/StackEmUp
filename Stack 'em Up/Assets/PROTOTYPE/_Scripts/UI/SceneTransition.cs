fbdc02e941a0f865db042953de04e4f005519952
Jaesda Kim <signupdump1@gmail.com>
2019-07-02 14:46:36 +1200

    Added SceneTransition.cs

---
+++ Stack 'em Up/Assets/PROTOTYPE/_Scripts/UI/SceneTransition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Animator anim;
+
+    public IEnumerator LoadScene(string sceneName)
+    {
+        anim.SetTrigger("End");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
